5a617dc229be29ab8d866d34f77934ceb30837b5
고이슬 <rhdltmf2543@gmail.com>
2020-06-03 15:41:14 +0900

    모바일버전추가

--- script/main.js
+++ script/main.js
@@ -6,16 +6,12 @@ function nav(){
     $('#nav_btn').click(function(){
         if($(this).hasClass('on')){
             $(this).removeClass('on');
-            $('#main_nav_wrap').animate({
-                right:-275
-            });
+            $('#main_nav_wrap').removeClass('active');
             $('#bar1').removeClass('bar1_on');
             $('#bar2').removeClass('bar2_on');
         } else{
             $(this).addClass('on');
-            $('#main_nav_wrap').animate({
-                right:0
-            });
+            $('#main_nav_wrap').addClass('active');
             $('#bar1').addClass('bar1_on');
             $('#bar2').addClass('bar2_on');
         }
