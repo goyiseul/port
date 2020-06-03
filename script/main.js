c575a00ad3966d81129967d03c73292466b00e4e
고이슬 <rhdltmf2543@gmail.com>
2020-06-03 17:36:03 +0900

    스크립트수정

--- script/main.js
+++ script/main.js
@@ -1,19 +1,30 @@
 $(function(){
     nav();
+    fadeNav();
 })
 
 function nav(){
     $('#nav_btn').click(function(){
         if($(this).hasClass('on')){
+            $('#fadeIn').fadeOut();
             $(this).removeClass('on');
             $('#main_nav_wrap').removeClass('active');
             $('#bar1').removeClass('bar1_on');
             $('#bar2').removeClass('bar2_on');
         } else{
+            $('#fade_nav').fadeIn();
             $(this).addClass('on');
             $('#main_nav_wrap').addClass('active');
             $('#bar1').addClass('bar1_on');
             $('#bar2').addClass('bar2_on');
         }
+    })
+}
+function fadeNav(){
+    $('#fade_nav').click(function(){
+        $('#nav_btn').removeClass('on');
+        $('#main_nav_wrap').removeClass('active');
+        $('#bar1').removeClass('bar1_on');
+        $('#bar2').removeClass('bar2_on');
     })
 }
